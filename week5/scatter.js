5641a7902cf13a8a059f5fecaa059b6d2684c4ec
quintenbecker <quintenworks@gmail.com>
2018-11-29 15:22:13 +0100

    week5 opdrachten

---
+++ week5/scatter.js
@@ -0,0 +1,72 @@
+window.onload = function() {
+
+  var consConf = "http://stats.oecd.org/SDMX-JSON/data/HH_DASH/FRA+DEU+KOR+NLD+PRT+GBR.COCONF.A/all?startTime=2007&endTime=2015"
+
+  var requests = d3.json(consConf);
+
+  Promise.resolve(requests).then(function(data) {
+      console.log(transformResponse(data));
+  }).catch(function(e){
+      throw(e);
+
+  });
+};
+
+function transformResponse(data){
+
+// access data property of the response
+let dataHere = data.dataSets[0].series;
+
+// access variables in the response and save length for later
+let series = data.structure.dimensions.series;
+let seriesLength = series.length;
+
+// set up array of variables and array of lengths
+let varArray = [];
+let lenArray = [];
+
+series.forEach(function(serie){
+    varArray.push(serie);
+    lenArray.push(serie.values.length);
+});
+
+// get the time periods in the dataset
+let observation = data.structure.dimensions.observation[0];
+
+// add time periods to the variables, but since it's not included in the
+// 0:0:0 format it's not included in the array of lengths
+varArray.push(observation);
+
+// create array with all possible combinations of the 0:0:0 format
+let strings = Object.keys(dataHere);
+
+// set up output array, an array of objects, each containing a single datapoint
+// and the descriptors for that datapoint
+let dataArray = [];
+
+// for each string that we created
+strings.forEach(function(string){
+    // for each observation and its index
+    observation.values.forEach(function(obs, index){
+        let data = dataHere[string].observations[index];
+        if (data != undefined){
+
+            // set up temporary object
+            let tempObj = {};
+
+            let tempString = string.split(":").slice(0, -1);
+            tempString.forEach(function(s, indexi){
+                tempObj[varArray[indexi].name] = varArray[indexi].values[s].name;
+            });
+
+            // every datapoint has a time and ofcourse a datapoint
+            tempObj["time"] = obs.name;
+            tempObj["datapoint"] = data[0];
+            dataArray.push(tempObj);
+        }
+    });
+});
+
+// return the finished product!
+return dataArray;
+}
