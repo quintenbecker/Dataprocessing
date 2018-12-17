5bf27d56e7d15a449285809e4dfecb23b1a12b88
quintenbecker <quintenworks@gmail.com>
2018-12-17 14:12:06 +0100

    rat

--- week6/linked.js
+++ week6/linked.js
@@ -6,14 +6,13 @@ svgBars = makeSvg()
 svgPie = makeSvgPie()
 
 
-
 }
-
-  var w = 700;
-  var h = 350;
 
   // make svg (canvas) element
   function makeSvg(){
+
+  var w = 700;
+  var h = 350;
 
   var barSvg = d3.select("body")
               .append("svg")
@@ -24,17 +23,21 @@ svgPie = makeSvgPie()
               return barSvg;
 }
 
-var width = 500;
-var height = 350;
 
 // make svg (canvas) element
   function makeSvgPie(){
+
+  var width = 500;
+  var height = 400;
+
 
   var pieSvg = d3.select("body")
               .append("svg")
               .attr("width", width)
               .attr("height", height)
-              .style("background", "grey")
+              // .style("background", "grey")
+              .attr("id", "piechart")
+
               return pieSvg;
 }
 
@@ -44,18 +47,58 @@ d3.json("jsonfile.json").then(function(data)
   countriesList = []
   countries = Object.keys(data)
 
+  womenList = []
+  women = Object.values(data)
+  women.forEach(function(a){
+    womenList.push(a.Women)
+  })
+
+  menList = []
+  men = Object.values(data)
+  men.forEach(function(b){
+    menList.push(b.Men)
+  })
+
+  pieDict = []
+  //
+  // for (i in range(len(countries))){
+  //    pieDict.push({"countries[i]"": menList[i], womenList[i]}
+
   totalList = []
   totals = Object.values(data)
   totals.forEach(function(d){
     totalList.push(d.Total)
   })
 
-barchart(totalList, countries)
+  barchart(totalList, countries)
+  piechart(womenList, menList)
 
 });
 
+  function piechart(w, women, men){
+    var piedata = [{gender: "men", percentage: men}, {gender: "women", percentage: women}]
 
-  function barchart(t, b){
+    var data = d3.pie().sort(null).value(function(d){return d.percentage;})(piedata);
+
+    var segments = d3.arc()
+                    .innerRadius(40)
+                    .outerRadius(150)
+
+    var sections =  svgPie.append("g")
+                    .attr("transform", "translate(300, 250)")
+                    .selectAll("path").data(data)
+
+
+    sections.enter().append("path").attr("d", segments).attr("fill", function(d){
+      if (d.data.gender == "men"){
+        return "navy";
+      }
+      return "pink";
+    });
+
+  }
+
+  function barchart(t, c){
 
     var w = 700;
     var h = 350;
@@ -74,13 +117,14 @@ barchart(totalList, countries)
     var tool_tip = d3.tip()
          .attr("class", "d3-tip")
          .offset([-8, 0])
-         .html(function(d, i) { return b[i] + ": "+ d});
+         .html(function(d, i) { return c[i] + ": "+ d});
     svgBars.call(tool_tip);
 
 
     var xScale = d3.scaleBand()
       .range([w, marginLeft])
-      .domain(b)
+      .domain(c)
+      // .attr("transform", "rotate(90)")
 
     var xAxis = svgBars.append('g')
           .attr("transform", "translate(" + "0" + "," + (h - marginbottom) +")")
@@ -97,8 +141,7 @@ barchart(totalList, countries)
       .style("display", "true")
       .style("fill", '#99e499')
 
-
-    console.log(t);
+    // console.log(t);
     svg = d3.select("body").append("svg")
     var rects = svgBars.selectAll("rect")
                    .data(t)
@@ -109,7 +152,7 @@ barchart(totalList, countries)
                     return yScale(d);
                     })
                    .attr("x", function(d, i) {
-                         return xScale(b[i]);
+                         return xScale(c[i]);
                        })
                    .attr("width", (w / t.length) - barPadding)
                    .attr("height", function(d) {
@@ -119,4 +162,24 @@ barchart(totalList, countries)
                    .attr("fill", "teal")
                    .on('mouseover', tool_tip.show)
                    .on('mouseout', tool_tip.hide)
+                   .on("click", function(d, i){
+                     piechart(d, menList[i], womenList[i])
+                   })
+     // append name of y axis
+     svgBars.append("text")
+     .attr("class", "ytext")
+     .style("font-size", "14px")
+     .attr("x", 250)
+     .attr("y", 20)
+     .text("% of adult education (Tertiary) level per country");
+
+
+     svgPie.append("text")
+     .attr("class", "ytext")
+     .style("font-size", "14px")
+     .attr("x", 200)
+     .attr("y", 65)
+     .text("% of the all men and woman");
+
+
 }
